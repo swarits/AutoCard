65f6a4bbdc65173f070ab9d65ae597fe9ba1ce17
Swarit Sharma <swarits@swarits.alacriti.com>
2019-06-27 15:07:08 +0530

    *(top-nav{delete cookie on signout})

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { SnackBarService } from 'src/app/services/snack-bar.service';
 
 @Component({
   selector: 'app-top-nav',
@@ -7,13 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cookieService: CookieService, private router: Router, private snackBar: SnackBarService) { }
 
   ngOnInit() {
   }
 
-  logout() {
+  signOut() {
     //clear cookies and call api
+    this.cookieService.delete("session");
+    this.router.navigateByUrl('/login');
+    this.snackBar.openSnackBar("Successfully logged out.", "");
   }
 
 }
